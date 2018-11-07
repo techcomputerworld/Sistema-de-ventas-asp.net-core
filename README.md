14c0e03b8a7b598c7117d30c704c7cd21a3a7f20
techcomputerworld <techcworld@outlook.com>
2018-11-07 13:44:29 +0100

    Problema en el video 8

---
+++ README.md
@@ -0,0 +1,13 @@
+# Curso de sistema de ventas de ASP.NET Core el profesor es Alex Pagoada
+
+[Curso Alex Pagoada]:https://www.youtube.com/watch?v=8Vjd6Ls6qtU&list=PLfkODrpjGnhnIJvlTpqrNwDtxETA1SjFr&index=1
+
+## Mi plan personal es una vez que de este curso y compore el curso y sepa como manejarlo ASP.NET Core.
+
+### Plan 
+
+1- crear el proyecto con otras bases de datos MySQL/MariaDB o PostgreSQL por ser software libre. 
+
+2- modificar las vistas a mi gusto y a lo mejor algunas funcionalidades de la aplicación web. 
+
+3- ya veré los cambios que implemento según vaya viendo el curso y aprendiendo.
---
+++ Sistem_Ventas/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Sistem_Ventas.Models;
+
+namespace Sistem_Ventas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IServiceProvider serviceProvider)
+        {
+            CreateRoles(serviceProvider);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        //metodo para crear los roles asincronico 
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            string mensaje;
+            try
+            {
+                var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                //a la hora de crear los roles, quiero introducir SuperAdmin, Admin, Manager por zona, User
+                String[] rolesName = { "Admin", "User" };
+                foreach (var item in rolesName)
+                {
+                    //esto nos sirve para verificar que cada rol existe en la tabla de roles 
+                    //es un metodo asincronico que le indicamos que mientras no acabe este metodo, no puede acabar el CreateRoles
+                    var roleExist = await roleManager.RoleExistsAsync(item);
+                    if (!roleExist)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(item));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+            
+
+        }
+    }
+}
